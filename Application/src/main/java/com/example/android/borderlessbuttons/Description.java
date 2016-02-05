0d01a3bae2d21955898fd1a5e42cfbf9dfe04bd3
AmberJMLee <alphagirl93@yahoo.com>
2016-02-05 17:28:49 -0500

    Initial commit

---
+++ Application/src/main/java/com/example/android/borderlessbuttons/Description.java
@@ -0,0 +1,15 @@
+package com.example.android.borderlessbuttons;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+/**
+ * Created by Owner on 2/5/2016.
+ */
+public class Description extends ListActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.item_description);
+    }
+}
