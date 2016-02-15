0ec524444ee4766f83287984caeff45dd8667ff5
AmberJMLee <alphagirl93@yahoo.com>
2016-02-14 20:39:29 -0500

    Added better UI.

--- Application/src/main/java/com/example/android/borderlessbuttons/Info.java
+++ Application/src/main/java/com/example/android/borderlessbuttons/Info.java
@@ -45,7 +45,36 @@ public class Info extends Activity{
             button.setImageResource(R.drawable.box);
         }
         else {
-            button.setImageResource(R.drawable.ic_launcher);
+            if (position == 0 || position == 11) {
+                button.setImageResource(R.drawable.brittnay); //Changes the button to purple thing
+            }
+            if (position == 1 || position == 12) {
+                button.setImageResource(R.drawable.deandra);
+            }
+            if (position == 2 || position == 13) {
+                button.setImageResource(R.drawable.jenna);
+            }
+            if (position == 3 || position == 14) {
+                button.setImageResource(R.drawable.mckenzie);
+            }
+            if (position == 4 || position == 15) {
+                button.setImageResource(R.drawable.rachel);
+            }
+            if (position == 5 || position == 16) {
+                button.setImageResource(R.drawable.shay);
+            }
+            if (position == 6 || position == 17 || position == 10) {
+                button.setImageResource(R.drawable.taylor);
+            }
+            if (position == 7 || position == 18) {
+                button.setImageResource(R.drawable.trisha);
+            }
+            if (position == 8 || position == 19) {
+                button.setImageResource(R.drawable.trisha2);
+            }
+            if (position == 9 || position == 20) {
+                button.setImageResource(R.drawable.saison);
+            }
         }
 
         position = intent.getIntExtra("POSITION", 0);
@@ -61,13 +90,13 @@ public class Info extends Activity{
     }
     public void setCompleteStatus(View view) {
         ImageButton button = (ImageButton) view.findViewById(R.id.task_completed);
-        number++;
+
         SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
         SharedPreferences.Editor editor = settings.edit();
         Intent intent = new Intent(this, MainActivity.class);
-
+        int isDone = settings.getInt(String.valueOf(position), 0);
         TextView text = (TextView) findViewById(R.id.complete);
-        if (number % 2 == 1) {
+        if (isDone == 0) {
 
             text.setText("TASK COMPLETE! CONGRATULATIONS!");
             editor.putInt(String.valueOf(String.valueOf(position)), 1);
